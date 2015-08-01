45b508e71c6c86d04d1c9dd6169b4405c02e2826
puzzlehawk <dcrypt@safe-mail.net>
2015-08-01 12:56:58 +0200

    rdrand entropy source for fortuna

---
+++ source/dcrypt/crypto/random/fortuna/sources/rdrand.d
@@ -0,0 +1,41 @@
+﻿module dcrypt.crypto.random.fortuna.sources.rdrand;
+
+import dcrypt.crypto.random.rdrand;
+import dcrypt.crypto.random.fortuna.entropysource;
+import dcrypt.crypto.random.fortuna.fortuna: addEntropy;
+
+/// Generate entropy data with intel rdrand instruction.
+
+@safe
+public class RDRandEntropySource: EntropySource
+{
+
+	private {
+		uint delay = 250;
+		RDRand rdrand;
+	}
+
+	override ubyte[] getEntropy(ubyte[] buf) {
+
+		if(rdrand.isSupported) {
+			rdrand.nextBytes(buf);
+			return buf;
+		} else {
+			// not supported
+			delay = 0;
+			return buf[0..0];
+		}
+
+	}
+
+	@nogc @property nothrow
+	override public string name() {
+		return "RDRand";
+	}
+
+	@safe @nogc nothrow
+	override uint scheduleNext() {
+		return delay;
+	}
+
+}
