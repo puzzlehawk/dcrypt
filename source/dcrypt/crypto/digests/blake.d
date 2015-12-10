24a054668e4df628db9f3328a7b9b95571c4c629
puzzlehawk <dcrypt@safe-mail.net>
2015-12-10 21:20:01 +0100

    put / absorb

--- source/dcrypt/crypto/digests/blake.d
+++ source/dcrypt/crypto/digests/blake.d
@@ -10,26 +10,20 @@ struct Blake(uint bitLength) {
 		alias ulong Word;
 	}
 
-	Word[8] h = iv;
-
-	Word[16]	state;
-	Word[4]		salt;
-	Word[2]		counter;
-
 	// initial values
 	static if(bitLength == 256) {
 		static immutable Word[8] iv = [
 			0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
 			0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
 		];
-
+		
 		static immutable Word[16] cst = [
 			0x243F6A88, 0x85A308D3, 0x13198A2E, 0x03707344,
 			0xA4093822, 0x299F31D0, 0x082EFA98, 0xEC4E6C89,
 			0x452821E6, 0x38D01377, 0xBE5466CF, 0x34E90C6C,
 			0xC0AC29B7, 0xC97C50DD, 0x3F84D5B5, 0xB5470917
 		];
-
+		
 	} else static if(bitLength == 384) {
 		enum Word
 			iv1 = 0xcbbb9d5dc1059ed8,
@@ -53,7 +47,7 @@ struct Blake(uint bitLength) {
 	} else {
 		static assert(false, "invalid bitlength");
 	}
-
+	
 	static immutable uint[16][14] perm = [
 		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
 		[14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
@@ -65,14 +59,54 @@ struct Blake(uint bitLength) {
 		[13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
 		[6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
 		[10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
-
+		
 		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
 		[14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
 		[11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
 		[7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8]
 	];
 
-	private static void compress(ref Word[8] h, in ref Word[16] m, in ref Word[4] salt, in ref Word[2] ctr) {
+	private {
+		Word[8] h = iv;
+
+		Word[4]		salt = 0;
+		Word[2]		counter;
+
+		ubyte[Word.sizeof * 16] buf;
+		size_t bufPtr;
+	}
+
+	invariant {
+		assert(bufPtr <= buf.length, "bufPtr out of range.");
+	}
+
+	public void put(const (ubyte)[] input...) {
+		// TODO optimize
+		while(input.length >= buf.length-bufPtr) {
+			buf[bufPtr..$] = input[0..buf.length-bufPtr];
+			input = input[buf.length-bufPtr..$];
+			absorb();
+		}
+		if(input.length > 0) {
+			assert(input.length < buf.length-bufPtr);
+			buf[bufPtr..bufPtr+input.length] = input[];
+		}
+	}
+
+	private void absorb() {
+
+		Word[16] msg;
+		fromBigEndian(buf, msg);
+
+		compress(h, msg, salt, ctr);
+
+		// increment counter
+
+		bufPtr = 0;
+		buf[] = 0;
+	}
+
+	private static void compress(ref Word[8] h, in ref Word[16] m, in ref Word[4] salt, in ref Word[2] ctr) pure nothrow @nogc @safe {
 		Word[16] state;
 
 		// initialize
@@ -116,7 +150,7 @@ struct Blake(uint bitLength) {
 		b = ror(b^c, 7);
 	}
 
-
+	
 }
 
 /// Test BLAKE round function.
@@ -127,28 +161,28 @@ private unittest {
 	fromBigEndian!Word(cast(const ubyte[]) x"
 			00800000  00000000  00000000  00000000  00000000  00000000  00000000  00000000
 			00000000  00000000  00000000  00000000  00000000  00000001  00000000  00000008", 
-			msg
+		msg
 		);
 
 	Word[16] v0;
 	fromBigEndian!Word(cast(const ubyte[]) x"
 			6A09E667  BB67AE85  3C6EF372  A54FF53A  510E527F  9B05688C  1F83D9AB  5BE0CD19
 			243F6A88  85A308D3  13198A2E  03707344  A409382A  299F31D8  082EFA98  EC4E6C89", 
-			v0
+		v0
 		);
 
 	Word[16] expected1;
 	fromBigEndian!Word(cast(const ubyte[]) x"
 			E78B8DFE  150054E7  CABC8992  D15E8984  0669DF2A  084E66E3  A516C4B3  339DED5B
 			26051FB7  09D18B27  3A2E8FA8  488C6059  13E513E6  B37ED53E  16CAC7B9  75AF6DF6", 
-			expected1
+		expected1
 		);
 
 	Word[16] expected2;
 	fromBigEndian!Word(cast(const ubyte[]) x"
 			9DE875FD  8286272E  ADD20174  F1B0F1B7  37A1A6D3  CF90583A  B67E00D2  943A1F4F
 			E5294126  43BD06BF  B81ECBA2  6AF5CEAF  4FEB3A1F  0D6CA73C  5EE50B3E  DC88DF91", 
-			expected2
+		expected2
 		);
 
 	Word[16] v = v0;
@@ -168,7 +202,7 @@ private unittest {
 	Word[8] expectedH;
 	fromBigEndian!Word(cast(const ubyte[]) x"
 			0CE8D4EF 4DD7CD8D 62DFDED9 D4EDB0A7 74AE6A41 929A74DA 23109E8F 11139C87", 
-			expectedH
+		expectedH
 		);
 
 	assert(h == expectedH, "BLAKE round failed!");
