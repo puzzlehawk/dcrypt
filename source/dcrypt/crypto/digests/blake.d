30ef1ddbf1406ca35253b1e0258cd911e1c6c06d
puzzlehawk <dcrypt@safe-mail.net>
2015-12-16 19:30:10 +0100

    template API compatibility

--- source/dcrypt/crypto/digests/blake.d
+++ source/dcrypt/crypto/digests/blake.d
@@ -1,97 +1,119 @@
 ﻿module dcrypt.crypto.digests.blake;
 
+/// Implementation of the BLAKE SHA-3 proposal.
+///
+
+public import dcrypt.crypto.digest;
 import dcrypt.bitmanip: ror, fromBigEndian, toBigEndian;
 import dcrypt.util: wipe;
+import std.conv: text;
 
-struct Blake(uint bitLength) {
+alias Blake!224 Blake224;
+alias Blake!256 Blake256;
+alias Blake!384 Blake384;
+alias Blake!512 Blake512;
 
-	static if(bitLength <= 256) {
-		alias uint Word;
-	} else {
-		alias ulong Word;
-	}
+static assert(isDigest!Blake224, "Blake224 does not fullfill requirements of isDigest.");
+static assert(isDigest!Blake256, "Blake256 does not fullfill requirements of isDigest.");
+static assert(isDigest!Blake384, "Blake384 does not fullfill requirements of isDigest.");
+static assert(isDigest!Blake512, "Blake512 does not fullfill requirements of isDigest.");
 
-	// Set IV.
-	static if(bitLength == 224) {
+@safe
+struct Blake(uint bitLength)
+if(bitLength == 224 || bitLength == 256 || bitLength == 384 || bitLength == 512) {
 
-		static immutable Word[8] iv = [
-			0xC1059ED8, 0x367CD507, 0x3070DD17, 0xF70E5939,
-			0xFFC00B31, 0x68581511, 0x64F98FA7, 0xBEFA4FA4
-		];
-
-	} else static if(bitLength == 256) {
-
-		static immutable Word[8] iv = [
-			0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
-			0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
-		];
-		
-	} else static if(bitLength == 384) {
-
-		static immutable Word[8] iv = [
-			0xcbbb9d5dc1059ed8, 0x629a292a367cd507, 0x9159015a3070dd17, 0x152fecd8f70e5939,
-			0x67332667ffc00b31, 0x8eb44a8768581511, 0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4
-		];
-
-	} else static if(bitLength == 512) {
-
-		static immutable Word[8] iv = [
-			0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
-			0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
-		];
-
-	} else {
-		static assert(false, "Invalid bit length.");
-	}
-
-	// Set constants and number of rounds.
-	static if(bitLength == 224 || bitLength == 256) {
-		
-		enum rounds = 14;
-
-		static immutable Word[16] cst = [
-			0x243F6A88, 0x85A308D3, 0x13198A2E, 0x03707344,
-			0xA4093822, 0x299F31D0, 0x082EFA98, 0xEC4E6C89,
-			0x452821E6, 0x38D01377, 0xBE5466CF, 0x34E90C6C,
-			0xC0AC29B7, 0xC97C50DD, 0x3F84D5B5, 0xB5470917
-		];
-
-	} else static if(bitLength == 384 || bitLength == 512) {
-		
-		enum rounds = 16;
-
-		static immutable Word[16] cst = [
-			0x243F6A8885A308D3, 0x13198A2E03707344, 0xA4093822299F31D0, 0x082EFA98EC4E6C89,
-			0x452821E638D01377, 0xBE5466CF34E90C6C, 0xC0AC29B7C97C50DD, 0x3F84D5B5B5470917,
-			0x9216D5D98979FB1B, 0xD1310BA698DFB5AC, 0x2FFD72DBD01ADFB7, 0xB8E1AFED6A267E96,
-			0xBA7C9045F12C7F99, 0x24A19947B3916CF7, 0x0801F2E2858EFC16, 0x636920D871574E69
-		];
-
-	} else {
-		static assert(false, "Invalid bit length.");
-	}
-	
-	static immutable uint[16][16] perm = [
-		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-		[14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
-		[11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
-		[7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
-		[9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
-		[2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
-		[12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
-		[13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
-		[6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
-		[10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
-		
-		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-		[14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
-		[11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
-		[7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
-		[9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
-		[2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9]
-	];
+	enum digestLength = bitLength / 8;
+	enum name = text("Blake", bitLength);
+	enum blockSize = 0;
 
 	private {
+
+		static if(bitLength <= 256) {
+			alias uint Word;
+		} else {
+			alias ulong Word;
+		}
+
+		// Set IV.
+		static if(bitLength == 224) {
+
+			static immutable Word[8] iv = [
+				0xC1059ED8, 0x367CD507, 0x3070DD17, 0xF70E5939,
+				0xFFC00B31, 0x68581511, 0x64F98FA7, 0xBEFA4FA4
+			];
+
+		} else static if(bitLength == 256) {
+
+			static immutable Word[8] iv = [
+				0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+				0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
+			];
+			
+		} else static if(bitLength == 384) {
+
+			static immutable Word[8] iv = [
+				0xcbbb9d5dc1059ed8, 0x629a292a367cd507, 0x9159015a3070dd17, 0x152fecd8f70e5939,
+				0x67332667ffc00b31, 0x8eb44a8768581511, 0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4
+			];
+
+		} else static if(bitLength == 512) {
+
+			static immutable Word[8] iv = [
+				0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
+				0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
+			];
+
+		} else {
+			static assert(false, "Invalid bit length.");
+		}
+
+		// Set constants and number of rounds.
+		static if(bitLength == 224 || bitLength == 256) {
+			
+			enum rounds = 14;
+
+			static immutable Word[16] cst = [
+				0x243F6A88, 0x85A308D3, 0x13198A2E, 0x03707344,
+				0xA4093822, 0x299F31D0, 0x082EFA98, 0xEC4E6C89,
+				0x452821E6, 0x38D01377, 0xBE5466CF, 0x34E90C6C,
+				0xC0AC29B7, 0xC97C50DD, 0x3F84D5B5, 0xB5470917
+			];
+
+		} else static if(bitLength == 384 || bitLength == 512) {
+			
+			enum rounds = 16;
+
+			static immutable Word[16] cst = [
+				0x243F6A8885A308D3, 0x13198A2E03707344, 0xA4093822299F31D0, 0x082EFA98EC4E6C89,
+				0x452821E638D01377, 0xBE5466CF34E90C6C, 0xC0AC29B7C97C50DD, 0x3F84D5B5B5470917,
+				0x9216D5D98979FB1B, 0xD1310BA698DFB5AC, 0x2FFD72DBD01ADFB7, 0xB8E1AFED6A267E96,
+				0xBA7C9045F12C7F99, 0x24A19947B3916CF7, 0x0801F2E2858EFC16, 0x636920D871574E69
+			];
+
+		} else {
+			static assert(false, "Invalid bit length.");
+		}
+		
+		static immutable uint[16][16] perm = [
+			[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+			[14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
+			[11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
+			[7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
+			[9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
+			[2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
+			[12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
+			[13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
+			[6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
+			[10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
+			
+			[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+			[14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
+			[11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
+			[7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
+			[9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
+			[2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9]
+		];
+
 		Word[8] h = iv;
 
 		Word[4]		salt = 0;
@@ -99,6 +121,13 @@ struct Blake(uint bitLength) {
 
 		ubyte[Word.sizeof * 16] buf = 0;
 		size_t bufPtr = 0;
+	}
+
+	public void start() nothrow @nogc @safe {
+		h = iv;
+		salt[] = 0;
+		buf[] = 0;
+		bufPtr = 0;
 	}
 
 	~this() {
@@ -109,7 +138,7 @@ struct Blake(uint bitLength) {
 		assert(bufPtr <= buf.length, "bufPtr out of range.");
 	}
 
-	public void put(const (ubyte)[] input...) {
+	public void put(const (ubyte)[] input...) nothrow @nogc @safe {
 		// TODO optimize
 		while(input.length >= buf.length-bufPtr) {
 			buf[bufPtr..$] = input[0..buf.length-bufPtr];
@@ -123,7 +152,7 @@ struct Blake(uint bitLength) {
 		}
 	}
 
-	public ubyte[bitLength / 8] finish() {
+	public ubyte[digestLength] finish() nothrow @nogc @safe {
 
 		// pad to 447 bits (message || 1 || 0...0 || 1) 
 		// at least 1 byte padding + 2*Word.sizeof bytes for length
@@ -158,12 +187,12 @@ struct Blake(uint bitLength) {
 		toBigEndian(counter[0], buf[$-Word.sizeof..$]);
 		absorb();
 
-		ubyte[bitLength / 8] hn;
-		toBigEndian(h[0..hn.length/Word.sizeof], hn[]);
+		ubyte[digestLength] hn;
+		toBigEndian(h[0..digestLength/Word.sizeof], hn[]);
 		return hn;
 	}
 
-	private void absorb(in uint bits = 0) {
+	private void absorb(in uint bits = 0) nothrow @nogc @safe {
 		Word[16] msg;
 		fromBigEndian(buf, msg);
 
@@ -206,7 +235,7 @@ struct Blake(uint bitLength) {
 		assert(blake.counter == [42, 3]);
 	}
 
-	private static void compress(ref Word[8] h, in ref Word[16] m, in ref Word[4] salt, in ref Word[2] ctr) {
+	private static void compress(ref Word[8] h, in ref Word[16] m, in ref Word[4] salt, in ref Word[2] ctr) pure nothrow @nogc @safe {
 		Word[16] state;
 
 		// initialize
@@ -239,7 +268,7 @@ struct Blake(uint bitLength) {
 		G!7(r, m, state[3], state[4], state[9], state[14]);
 	}
 
-	private static void G(uint i, Word)(in size_t r, in ref Word[16] msg, ref Word a, ref Word b, ref Word c, ref Word d) pure nothrow @nogc @safe {
+	private static void G(uint i)(in size_t r, in ref Word[16] msg, ref Word a, ref Word b, ref Word c, ref Word d) pure nothrow @nogc @safe {
 
 		static if(Word.sizeof == 4) {
 			// rotation distances for Blake224 and Blake256
