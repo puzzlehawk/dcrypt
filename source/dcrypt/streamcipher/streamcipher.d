704d7b55e82df7f516201c878aeba3f1a4de4b70
puzzlehawk <dcrypt@safe-mail.net>
2016-05-08 10:39:39 +0200

    Stream cipher: removed reset, returnByte

--- source/dcrypt/streamcipher/streamcipher.d
+++ source/dcrypt/streamcipher/streamcipher.d
@@ -1,7 +1,7 @@
 module dcrypt.streamcipher.streamcipher;
 
 
-/// test if struct is a stream cipher
+/// Test if struct is a stream cipher.
 @safe
 template isStreamCipher(T)
 {
@@ -13,68 +13,36 @@ template isStreamCipher(T)
 						T c = void; //Can define
 						string name = c.name;
 						c.start(true, cast(const ubyte[]) block, cast(const ubyte[]) block); // init with key and IV
-						ubyte b = c.returnByte(cast(ubyte)0);
 						ubyte[] outSlice = c.processBytes(cast(const ubyte[]) block, block);
-						c.reset();
 					}));
 }
 
 @safe
 public interface IStreamCipher {
-	/**
-	 * Initialize the cipher.
-	 *
-	 * Params: forEncryption = if true the cipher is initialised for
-	 *  encryption, if false for decryption.
-	 *  params = the key and other data required by the cipher.	 
-	 * Throws: IllegalArgumentException if the params argument is
-	 * inappropriate.
-	 */
-	@safe
-	public void start(bool forEncryption, in ubyte[] key, in ubyte[] iv);
 
-	/**
-	 * Returns: the name of the algorithm the cipher implements.
-	 */
+	/// Initialize the cipher.
+	/// 
+	/// Params:
+	/// forEncryption	=	Encrypt if true, decrypt if false.
+	/// key	=	Secret key.
+	/// nonce =	Initialization vector.
+	@safe
+	public void start(bool forEncryption, in ubyte[] key, in ubyte[] nonce);
+
+	/// Returns: the name of the algorithm the cipher implements.
 	@safe
 	public string name() pure nothrow;
 
-	/**
-	 * encrypt/decrypt a single byte returning the result.
-	 *
-	 * Params: in the byte to be processed.
-	 * Returns: the result of processing the input byte.
-	 */
-	@safe
-	public ubyte returnByte(ubyte input);
-
-	/**
-	 * process a block of bytes from in putting the result into out.
-	 *
-	 * Params: input = the input byte array.
-	 * output = the output buffer the processed bytes go into.
-	 * Throws: BufferLengthException if the output buffer is too small.
-	 */
+	/// Process a block of bytes from in putting the result into out.
+	///
+	/// Params:
+	/// input = The input byte array.
+	/// output = The output buffer the processed bytes go into.
+	/// 
+	/// Returns:
+	/// Returns a slice pointing to the output data.
 	@safe
 	public ubyte[] processBytes(in ubyte[] input, ubyte[] output);
-	
-	@safe
-	public void reset();
-	
-	/**
-	 * process a block of bytes from in putting the result into out.
-	 *
-	 * Params: input = the input byte array.
-	 * Returns: slice containing the encrypted or decrypted data
-	 * Throws: BufferLengthException if the output buffer is too small.
-	 */
-	@safe
-	public final ubyte[] processBytes(in ubyte[] input) {
-		ubyte[] output = new ubyte[input.length];
-		processBytes(input, output);
-		return output;
-	}
-
 }
 
 @safe
@@ -97,40 +65,20 @@ public class StreamCipherWrapper(T) if(isStreamCipher!T): IStreamCipher {
 	public string name() pure nothrow {
 		return cipher.name;
 	}
+
 	
-	/**
-	 * encrypt/decrypt a single byte returning the result.
-	 *
-	 * Params: in = the byte to be processed.
-	 * Returns: the result of processing the input byte.
-	 */
-	@safe
-	public ubyte returnByte(ubyte input) {
-		return cipher.returnByte(input);
-	}
-	
-	/**
-	 * process a block of bytes from in putting the result into out.
-	 *
-	 * Params: input = the input byte array.
-	 * output = the output buffer the processed bytes go into.
-	 * 
-	 * Returns: Slice pointing to encrypted or decrypted data. Might be smaller than `output` buffer.
-	 * 
-	 * Throws: BufferLengthException if the output buffer is too small.
-	 * 
-	 */
+	/// Process a block of bytes from in putting the result into out.
+	///
+	/// Params:
+	/// input = The input byte array.
+	/// output = The output buffer the processed bytes go into.
+	/// 
+	/// Returns:
+	/// Returns a slice pointing to the output data.
 	@safe
 	public ubyte[] processBytes(in ubyte[] input, ubyte[] output) {
 		return cipher.processBytes(input, output);
 	}
-	
-	@safe
-	deprecated("The reset() function might lead to insecure use of a stream cipher.")
-	public void reset() {
-		cipher.reset();
-	}
-	
 }
 
 /// Use this to test a stream cipher with multiple keys, plaintexts and ivs.
